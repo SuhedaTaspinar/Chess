d101b382c011ba4a84a5eaa236f99eda8f6ef568
Şüheda <92635912+SuhedaTaspinar@users.noreply.github.com>
2024-08-23 22:14:27 +0300

    Set up React and installed chess.js, react-dnd, RxJS libraries, and Tailwind CSS.

---
+++ chess/src/App.js
@@ -0,0 +1,17 @@
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
+import Board from "./Board";
+import Game from "./Game";
+
+function App() {
+    console.log(Game, "game")
+    return (
+        <DndProvider backend={HTML5Backend}>
+            <div>
+                <Board/>
+            </div>
+        </DndProvider>
+    );
+}
+
+export default App;
